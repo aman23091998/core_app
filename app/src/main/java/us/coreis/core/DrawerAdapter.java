ea4b2987321974dccf3cf431f909069321d8ecb7
aman23091998 <aman23091998@gamil.com>
2015-09-30 19:28:51 +0530

    Navigation Bar Modified Now Options Clickable i.e. onClickListener set properly Removed default comments giving warnings Icons for Row Text Added

--- app/src/main/java/us/coreis/core/DrawerAdapter.java
+++ app/src/main/java/us/coreis/core/DrawerAdapter.java
@@ -1,5 +1,10 @@
 package us.coreis.core;
 
+import android.support.v4.app.Fragment;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.internal.view.menu.MenuView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,48 +17,110 @@ import java.util.List;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-/**
- * Created by aman on 30/9/15.
- */
 public class DrawerAdapter extends RecyclerView.Adapter<DrawerAdapter.ViewHolder> {
-    private static final int HEADER_TYPE=0;
-    private static final int ROW_TYPE=1;
+    private static final int HEADER_TYPE = 0;
+    private static final int ROW_TYPE = 1;
 
-    private List<String> rows;
+    String[] rows;
+    TypedArray icons;
+    Context context;
 
-    public DrawerAdapter(List<String> rows) {
+    DrawerAdapter(String[] rows , TypedArray icons , Context context) {
         this.rows=rows;
+        this.icons=icons;
+        this.context=context;
+    }
+
+
+
+    public static class ViewHolder extends  RecyclerView.ViewHolder implements View.OnClickListener {
+        protected int viewType ;
+        TextView  navTitle;
+        ImageView navIcon;
+        Context context;
+
+        public ViewHolder(View itemView , int viewType  ,Context context) {
+            super(itemView);
+            this.context = context;
+            this.viewType=viewType;
+            itemView.setOnClickListener(this);
+            if(viewType == ROW_TYPE){
+                navTitle = (TextView) itemView.findViewById(R.id.drawer_row_text);
+                navIcon = (ImageView) itemView.findViewById(R.id.drawer_row_icon);
+
+            }
+        }
+
+        @Override
+        public void onClick(View v) {
+
+            MainActivity mainActivity = (MainActivity) context;
+            mainActivity.drawerLayout.closeDrawers();
+            FragmentTransaction fragmentTransaction = mainActivity.getSupportFragmentManager().beginTransaction();
+
+            switch (getAdapterPosition()){
+                case 1:
+                    Fragment homeFragment = new HomeFragment();
+                    fragmentTransaction.replace(R.id.containerView,homeFragment);
+                    fragmentTransaction.commit();
+                    break;
+                case 2:
+                    Fragment eventsFragment = new EventsFragment();
+                    fragmentTransaction.replace(R.id.containerView,eventsFragment);
+                    fragmentTransaction.commit();
+                    break;
+                case 3:
+                    Fragment encoreFragment = new EncoreFragment();
+                    fragmentTransaction.replace(R.id.containerView,encoreFragment);
+                    fragmentTransaction.commit();
+                    break;
+                case 4:
+                    Fragment contactFragment = new ContactFragment();
+                    fragmentTransaction.replace(R.id.containerView,contactFragment);
+                    fragmentTransaction.commit();
+                    break;
+                case 5:
+                    Fragment teamFragment = new TeamFragment();
+                    fragmentTransaction.replace(R.id.containerView,teamFragment);
+                    fragmentTransaction.commit();
+                    break;
+                default:
+                    Fragment defaultFragment = new HomeFragment();
+                    fragmentTransaction.replace(R.id.containerView,defaultFragment);
+                    fragmentTransaction.commit();
+                    break;
+            }
+        }
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public DrawerAdapter.ViewHolder onCreateViewHolder (ViewGroup parent, int viewType) {
         if(viewType == HEADER_TYPE){
              View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.drawer_header , parent ,false);
-            return new ViewHolder(view ,viewType) ;
+            return new ViewHolder(view ,viewType,context) ;
         }
         else if (viewType == ROW_TYPE ){
             View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.drawer_row , parent ,false);
-            return new ViewHolder(view ,viewType) ;
+            return new ViewHolder(view ,viewType,context) ;
 
         }
         return null;
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public void onBindViewHolder(DrawerAdapter.ViewHolder holder, int position) {
        if(holder.viewType == ROW_TYPE){
-         String rowtext = rows.get (position -1 );
-           holder.textView.setText(rowtext);
-          /*
-          if you wish to have an image along with each row
-          holder.imageView.setImageResource(R.mipmap.ic_launcher);
-          */
+
+           holder.navTitle.setText(rows[position - 1]);
+           holder.navIcon.setImageResource(icons.getResourceId(position-1,-1));
+
        }
     }
 
     @Override
-    public int getItemCount() {
-        return rows.size()+1;
+    public int getItemCount()
+    {
+        return rows.length+1;
     }
 
     @Override
@@ -62,18 +129,4 @@ public class DrawerAdapter extends RecyclerView.Adapter<DrawerAdapter.ViewHolder
         return ROW_TYPE;
     }
 
-    public static class ViewHolder extends  RecyclerView.ViewHolder {
-        protected int viewType ;
-     /*
-        if including icons in each rom
-        @Bind(R.id.drawer_row_icon) ImageView imageView; */
-        @Bind(R.id.drawer_row_text) TextView textView;
-        public ViewHolder(View itemView , int viewType) {
-            super(itemView);
-            this.viewType=viewType;
-            if(viewType == ROW_TYPE){
-                ButterKnife.bind(this, itemView);
-            }
-        }
-    }
 }
--- app/src/main/java/us/coreis/core/MainActivity.java
+++ app/src/main/java/us/coreis/core/MainActivity.java
@@ -1,47 +1,58 @@
 package us.coreis.core;
 
-import android.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-/**
- * Created by aman on 29/9/15.
- */
 public class MainActivity extends AppCompatActivity {
     @Bind(R.id.drawer_layout) DrawerLayout drawerLayout;
     @Bind(R.id.toolbar)    Toolbar toolbar;
     @Bind(R.id.drawer_recyclerView)  RecyclerView drawerRecyclerView ;
+
+    ActionBarDrawerToggle drawerToggle ;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        drawerToggle= new ActionBarDrawerToggle(this ,drawerLayout ,toolbar ,R.string.app_name ,R.string.app_name );
         setSupportActionBar(toolbar);
-        ActionBarDrawerToggle drawerToggle= new ActionBarDrawerToggle(this ,drawerLayout ,toolbar ,R.string.app_name ,R.string.app_name );
         drawerLayout.setDrawerListener(drawerToggle);
         drawerToggle.syncState();
-        List <String> rows =new ArrayList<>();
-        rows.add("Home");
-        rows.add("Events");
-        rows.add("EnCOЯE");
-        rows.add("Contact Us");
-        rows.add("The Team");
-        DrawerAdapter drawerAdapter =new DrawerAdapter(rows);
+        String rows[];
+        TypedArray navIcons;
+
+
+        rows =getResources().getStringArray(R.array.navDrawerItems) ;
+
+        navIcons = getResources().obtainTypedArray(R.array.navDrawerIcons);
+
+        DrawerAdapter drawerAdapter =new DrawerAdapter(rows ,navIcons , this );
         drawerRecyclerView.setAdapter(drawerAdapter);
         drawerRecyclerView.hasFixedSize();
         drawerRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        drawerSwitch();                        //DrawerToggle
+
+        //Add the Very First i.e Home Fragment to the Container
+        Fragment homeFragment = new HomeFragment();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.containerView, homeFragment, null);
+        fragmentTransaction.commit();
+    }
+    void drawerSwitch(){
+        drawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.app_name,R.string.app_name);
+        //This is necessary to change the icon of the Drawer Toggle upon state change.
+        drawerToggle.syncState();
     }
 }
